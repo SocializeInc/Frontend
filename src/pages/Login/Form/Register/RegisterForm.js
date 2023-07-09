f6d097b69d9e29bf86e2e8ccc38df6974feddc1e
Darci01 <adam.dara@hotmail.com>
2023-07-10 00:13:37 +0200

    register auth from db implemented

--- src/pages/Login/Form/Register/RegisterForm.js
+++ src/pages/Login/Form/Register/RegisterForm.js
@@ -4,23 +4,29 @@ import Card from "../../../../components/UI/Card";
 import "./RegisterForm.css";
 
 const RegisterForm = (props) => {
+  const [isRegisterSubmitted, setRegisterIsSubmitted] = useState(false);
+
   const [registeredUserData, setRegisteredUserData] = useState({
-    firstName: "",
-    lastName: "",
     username: "",
+    firstname: "",
+    lastname: "",
     email: "",
     password: "",
+    passwordAgain: "",
+    country: "",
+    birthDate: "",
+    role: "",
   });
 
   const firstNameChangeHandler = (event) => {
     setRegisteredUserData((prevState) => {
-      return { ...prevState, firstName: event.target.value };
+      return { ...prevState, firstname: event.target.value };
     });
   };
 
   const lastNameChangeHandler = (event) => {
     setRegisteredUserData((prevState) => {
-      return { ...prevState, firstName: event.target.value };
+      return { ...prevState, lastname: event.target.value };
     });
   };
 
@@ -42,49 +48,104 @@ const RegisterForm = (props) => {
     });
   };
 
-  //TODO: Give JSON forward
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    if (!registeredUserData.firstName) {
-      console.log("First Name is empty");
-    } else if (!registeredUserData.lastName) {
-      console.log("Last name is empty");
-    }
-
-    const userData = {
-      firstName: registeredUserData.firstName,
-      lastName: registeredUserData.lastName,
-      username: registeredUserData.username,
-      email: registeredUserData.email,
-      password: registeredUserData.password,
-    };
-
-    //TODO: send userData JSON to backend
-    setRegisteredUserData({
-      firstName: "",
-      lastName: "",
-      username: "",
-      email: "",
-      password: "",
+  const passwordAgainChangeHandler = (event) => {
+    setRegisteredUserData((prevState) => {
+      return { ...prevState, passwordAgain: event.target.value };
     });
   };
 
+  const countryChangeHandler = (event) => {
+    setRegisteredUserData((prevState) => {
+      return { ...prevState, country: event.target.value };
+    });
+  };
+
+  const birthDateChangeHandler = (event) => {
+    setRegisteredUserData((prevState) => {
+      return { ...prevState, birthDate: event.target.value };
+    });
+  };
+
+  //Give JSON forward
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    (async () => {
+      await fetch("http://localhost:8080/socialize/api/auth/signup", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "post",
+        body: JSON.stringify({
+          username: registeredUserData.username,
+          firstname: registeredUserData.firstname,
+          lastname: registeredUserData.lastname,
+          email: registeredUserData.email,
+          password: registeredUserData.password,
+          country: registeredUserData.country,
+          birthDate: registeredUserData.birthDate,
+          role: ["adamdara", "user"],
+        }),
+      })
+        // .then((response) => response.json())
+        // .then((response) => {
+        //   response.accessToken
+        //     ? setRegisterIsSubmitted(true)
+        //     : setRegisterIsSubmitted(false);
+        // })
+        .then((response) => response.json())
+        .then((response) => {
+          response.message === "User registered successfully!"
+            ? setRegisterIsSubmitted(true)
+            : setRegisterIsSubmitted(false);
+        })
+        .catch((err) => console.log(err));
+    })();
+
+    // setRegisteredUserData({
+    //   username: "",
+    //   firstname: "",
+    //   lastname: "",
+    //   email: "",
+    //   password: "",
+    //   passwordAgain: "",
+    //   country: "",
+    //   birthDate: "",
+    //   role: new Set(),
+    // });
+
+    setRegisteredUserData({
+      username: registeredUserData.username,
+      firstname: registeredUserData.firstname,
+      lastname: registeredUserData.lastname,
+      email: registeredUserData.email,
+      password: registeredUserData.password,
+      country: registeredUserData.country,
+      birthDate: registeredUserData.birthDate,
+      role: "",
+    });
+  };
+
+  //TODO: Give user a successful register text
+  setTimeout(1000);
+  if (isRegisterSubmitted) {
+    props.onChangeRegister(false);
+  }
+
   const backToLoginHandler = (event) => {
     event.preventDefault();
-    props.onChangeRegister(false);
   };
 
   return (
     <Card>
-      <div className="title">Login</div>
+      <div className="title">Register</div>
       <form onSubmit={submitHandler}>
         <div className="input-container">
           <label>First Name</label>
           <input
             type="text"
             name="firstName"
-            value={registeredUserData.firstName}
+            value={registeredUserData.firstname}
             onChange={firstNameChangeHandler}
             required
           />
@@ -94,7 +155,7 @@ const RegisterForm = (props) => {
           <input
             type="text"
             name="lastName"
-            value={registeredUserData.lastName}
+            value={registeredUserData.lastname}
             onChange={lastNameChangeHandler}
             required
           />
@@ -135,7 +196,29 @@ const RegisterForm = (props) => {
           <input
             type="password"
             name="registerPassAgain"
+            value={registeredUserData.passwordAgain}
+            onChange={passwordAgainChangeHandler}
             minLength={6}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label>Country</label>
+          <input
+            type="text"
+            name="country"
+            value={registeredUserData.country}
+            onChange={countryChangeHandler}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label>Birth Date</label>
+          <input
+            type="text"
+            name="birthDate"
+            value={registeredUserData.birthDate}
+            onChange={birthDateChangeHandler}
             required
           />
         </div>
