c615a619f60e859435fb0931932adecd9acadb51
Darci01 <adam.dara@hotmail.com>
2023-07-09 23:03:13 +0200

    login auth implemented with backend

--- src/pages/Login/Auth.js
+++ src/pages/Login/Auth.js
@@ -4,18 +4,11 @@ import Form from '../../pages/Login/Form/Form';
 
 import './Auth.css';
 
-const database = [
-  {
-    username: "user1",
-    password: "pass1"
-  },
-];
-
 const Auth = () => {
   return (
     <div className="app">
       <h1 className="main_title">Socialize</h1>
-      <Form users={database} />
+      <Form/>
     </div>
   );
 };
